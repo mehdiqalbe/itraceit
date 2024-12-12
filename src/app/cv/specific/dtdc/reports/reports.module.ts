f8bd8c6ca3bf61120857c806f5ef4140f455b284
mehdiqalbe <qalbe.mehdi@wrmsglobal.com>
2024-12-12 12:23:58 +0530

    created report module

---
+++ src/app/cv/specific/dtdc/reports/reports.module.ts
@@ -0,0 +1,46 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ReportsRoutingModule } from './reports-routing.module';
+import { TripReportComponent } from './trip-report/trip-report.component';
+import { NgSelectModule } from '@ng-select/ng-select';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
+import { ClickOutsideModule } from 'ng-click-outside';
+import { ColorPickerModule } from 'ngx-color-picker';
+import { NgxDaterangepickerMd } from 'ngx-daterangepicker-material';
+import { NgxMaterialTimepickerModule } from 'ngx-material-timepicker';
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { NgMultiSelectDropDownModule } from 'ng-multiselect-dropdown';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { LeafletModule } from '@asymmetrik/ngx-leaflet';
+
+
+@NgModule({
+  declarations: [
+    TripReportComponent
+  ],
+  imports: [
+    CommonModule,
+    ReportsRoutingModule,
+    // CommonModule,
+    // DtdcRoutingModule,
+    NgSelectModule,
+    NgxDatatableModule,
+    // DatePipe,
+    // BrowserModule,
+    ClickOutsideModule,
+    ColorPickerModule,
+    NgxDaterangepickerMd,
+    NgxMaterialTimepickerModule,
+    Ng2SearchPipeModule,
+    NgbModule, 
+    // NgbNavOutlet,
+    NgxSpinnerModule,
+    NgMultiSelectDropDownModule,
+    FormsModule, ReactiveFormsModule,
+    LeafletModule
+  ]
+})
+export class ReportsModule { }
