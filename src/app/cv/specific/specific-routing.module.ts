a92411138068f30f5b5da722c8edece6c5bc53bc
saumyaSingh27 <saumya7607539419@gmail.com>
2024-12-12 11:47:42 +0530

    login change

--- src/app/cv/specific/specific-routing.module.ts
+++ src/app/cv/specific/specific-routing.module.ts
@@ -4,7 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: 'bluedart', loadChildren: () => import('./bluedart/bluedart.module').then(m => m.BluedartModule) },
   { path: 'dtdc', loadChildren: () => import('./dtdc/dtdc.module').then(m => m.DtdcModule) },
-  { path: '', redirectTo: 'bluedart', pathMatch: 'full' }
+  // { path: '', redirectTo: 'bluedart', pathMatch: 'full' }
 ];
 
 @NgModule({
